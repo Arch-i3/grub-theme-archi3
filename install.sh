14a613b83d3591d1bca578bfb7ad5c6217e01bf5
Sándor Kakuk <53403093+sandorkakuk@users.noreply.github.com>
2025-04-15 08:25:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-rm -rf /boot/grub/themes/grub-theme-archi3
-cp -TR ./theme /boot/grub/themes/gbub-theme-archi3
+rm -rf /boot/grub/themes/archi3
+cp -TR ./theme /boot/grub/themes/archi3
 grub-mkconfig -o /boot/grub/grub.cfg
